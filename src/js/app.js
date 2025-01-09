9b336e48691f916d01f0eb8a8c00b2225c7942e7
Sofya Ivanova <sofia146iv@gmail.com>
2025-01-09 10:06:53 +0300

    Import/Export

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
+import Game,
+{
+    GameSavingData as saveGame,
+    writeGameSaving as writeGame,
+    readGameSaving as loadGame
+} from './game.js';
 console.log('app worked')
+
+const game = new Game();
+game.start();
